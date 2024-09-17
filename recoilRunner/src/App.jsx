e582b62109b3b8500f6369ff7dc366b8c625cea6
Darshan Choudhary <97291483+DarshanchGIT@users.noreply.github.com>
2024-09-18 04:51:53 +0530

    selectors

--- recoilRunner/src/App.jsx
+++ recoilRunner/src/App.jsx
@@ -1,11 +1,17 @@
 // App.js
-import { RecoilRoot, useRecoilState, useRecoilValue } from "recoil";
-import { counterAtom } from "./store/atoms/count";
+import {
+  RecoilRoot,
+  // useRecoilState,
+  useRecoilValue,
+  useSetRecoilState,
+} from "recoil";
+
+import { counterAtom, countSelector } from "./store/atoms/count";
 import "./App.css";
 
 export default function App() {
   return (
-    // important to wrap the top level with the provider 
+    // important to wrap the top level with the provider
     // in this case, we'll wrap count with <RecoilRoot/>
     <>
       <RecoilRoot>
@@ -16,6 +22,7 @@ export default function App() {
 }
 
 const CountComp = () => {
+  console.log("CountComp rendered");
   return (
     <>
       <Count />
@@ -26,10 +33,14 @@ const CountComp = () => {
 
 const Count = () => {
   // Correctly using useRecoilValue to get the count value
+  console.log("Count rendered");
   const count = useRecoilValue(counterAtom);
+  const selectorValue = useRecoilValue(countSelector);
   return (
     <>
       <b>{count}</b>
+      {/* render "it's even whenever count values become even" */}
+      {selectorValue ? <h4>Count is even</h4> : null}
     </>
   );
 };
@@ -37,11 +48,19 @@ const Count = () => {
 //buttons
 const Button = () => {
   // Correctly using useRecoilState to both get and set the count value
-  const [count, setCount] = useRecoilState(counterAtom);
+  //technically either buttons aren't necessary to re-render
+  // const [count, setCount] = useRecoilState(counterAtom);
+  const setCount = useSetRecoilState(counterAtom);
+  console.log("Button rendered");
   return (
     <>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
-      <button onClick={() => setCount(count - 1)}>Decrement</button>
+      {/* And convert this setCount(count + 1) to setCount( prevCount => prevCount + 1) */}
+      <button onClick={() => setCount((prevCount) => prevCount + 1)}>
+        Increment
+      </button>
+      <button onClick={() => setCount((prevCount) => prevCount - 1)}>
+        Decrement
+      </button>
     </>
   );
 };
--- recoilRunner/src/store/atoms/count.jsx
+++ recoilRunner/src/store/atoms/count.jsx
@@ -1,5 +1,14 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 export const counterAtom = atom({
-  default: 0,
+  default: 1,
   key: "counterAtom",
+});
+
+//selectors
+export const countSelector = selector({
+  key: "countSelector",
+  get: ({ get }) => {
+    const darshan = get(counterAtom);
+    return darshan % 2 == 0;
+  },
 });
